7a56993f3faa0c3a4d19428de61a7f2d6f1df1ba
Jrozo <Jrozo@DESKTOP-031E2FB>
2020-05-05 18:52:33 -0500

    Creacion clase version 2

---
+++ Probando/src/probando/Version_2.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package probando;
+
+/**
+ *
+ * @author Jrozo
+ */
+public class Version_2 {
+ 
+    private int prueba;
+    //Esto es todo compañeros
+    
+    
+
+    
+}
